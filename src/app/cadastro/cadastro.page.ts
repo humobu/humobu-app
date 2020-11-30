35e96a7c28eb31452780b067c0d6d3d513f5a0e1
lucas.chinarelli <lucas.chinarelli@rpinfo.com.br>
2020-11-29 23:42:06 -0300

    Implementado tela de cadastro

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.page.html',
   styleUrls: ['./cadastro.page.scss'],
 })
-export class CadastroPage implements OnInit {
+export class CadastroPage {
+
+  senhaVisivel: boolean;
+
+  private usuario = {
+    email: '',
+    senha: ''
+  };
 
   constructor(
+    private angularFireAuth: AngularFireAuth,
     private translate: TranslateService
   ) { 
     this.translate.use('pr-br');
@@ -19,7 +28,16 @@ export class CadastroPage implements OnInit {
 
   }
 
-  ngOnInit() {
+  async exibirSenha(){
+    this.senhaVisivel = !this.senhaVisivel;
   }
-
+  
+  async cadastrar() {
+    try {
+      const usuarioCriado = await this.angularFireAuth.createUserWithEmailAndPassword(this.usuario.email, this.usuario.senha);
+      console.log(usuarioCriado);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -4,6 +4,7 @@ import { LoginFacebookService } from '../services/login-facebook.service';
 import { LoginTwitterService } from '../services/login-twitter.service';
 import { LoginGoogleService } from '../services/login-google.service';
 import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,12 +13,15 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class LoginPage {
 
+  senhaVisivel: boolean;
+  
   constructor(
     private loginFacebookService: LoginFacebookService,
     private loginTwitterService: LoginTwitterService,
     private loginGoogleService: LoginGoogleService,
     private toastController: ToastController,
-    private translate: TranslateService
+    private translate: TranslateService,
+    private router: Router
   ) { 
     this.translate.use('pr-br');
     this.translate.setDefaultLang('pt-br');
@@ -27,8 +31,12 @@ export class LoginPage {
 
   }
 
+  async exibirSenha(){
+    this.senhaVisivel = !this.senhaVisivel;
+  }
+
   async loginUsingFacebook() {
-    try {
+    try {      
       const login = await this.loginFacebookService.loginWithFacebook();
 
       console.log(login.additionalUserInfo.profile);
@@ -80,4 +88,20 @@ export class LoginPage {
       toast.present();
     }
   }
+
+  async cadastrar() {
+    try {
+      this.router.navigate(["cadastro"]);
+    } catch (error) {
+      console.error(error);
+
+      const toast = await this.toastController.create({
+        animated: true,
+        message: 'Erro ao realizar login com o Google, tente novamente',
+        duration: 3000
+      });
+
+      toast.present();
+    }
+  }  
 }
